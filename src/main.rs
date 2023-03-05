8ac0d2be491856c8a08bef1f9b979b42d574a30f
Hubaka Ghayati Setyakurnia <hubakag@gmail.com>
2023-03-05 13:41:36 +0700

    thirdcommit

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 fn main() {
-    let myName :String = "Hubaka".to_string();
-    let myAge = 25;
-    let myAgetoString = myAge.to_string();
-    let mut myHelloworld:String = ("Hello, world! My name is ").to_string();
-    myHelloworld.push_str(myName.as_str());
-    myHelloworld.push_str(", i am ");
-    myHelloworld.push_str(myAgetoString.as_str());
-    myHelloworld.push_str(" years old!");
-    println!("{}",myHelloworld);
+    let my_name:String = "Hubaka".to_string();
+    let my_age = 25;
+    let my_age_to_string = my_age.to_string();
+    let mut my_hello_world:String = ("Hello, world! My name is ").to_string();
+    my_hello_world.push_str(my_name.as_str());
+    my_hello_world.push_str(", i am ");
+    my_hello_world.push_str(my_age_to_string.as_str());
+    my_hello_world.push_str(" years old!");
+    println!("{}", my_hello_world);
     println!("");
-    let mut sellingcake :String = ("I am selling a cake.").to_string();
-    let mut cakeprice = 30;
-    println!("{}", sellingcake);
+    let mut selling_cake:String = ("I am selling a cake.").to_string();
+    let mut cake_price = 30;
+    println!("{}", selling_cake);
     print!("The price is ");
-    print!("{}",cakeprice);
+    print!("{}", cake_price);
     println!(" dollars.");
-    cakeprice = 50;
+    cake_price = 50;
     print!("After 5 months, the cake price is ");
-    print!("{}", cakeprice);
+    print!("{}", cake_price);
     print!(" dollars.");
 
 
